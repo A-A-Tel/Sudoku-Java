1dc8aacb3e7de109785dccd410d4d40dcea4dc7c
a-a-tel <anouaranthonytel@gmail.com>
2025-04-27 19:22:13 +0200

    working app

--- src/main/java/com/anthony/sudoku/Main.java
+++ src/main/java/com/anthony/sudoku/Main.java
@@ -1,7 +1,24 @@
 package com.anthony.sudoku;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
     public static void main(String[] args) {
-        System.out.println( "Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(label, 400, 200);
+
+        stage.setTitle("HelloFX");
+        stage.setScene(scene);
+        stage.show();
     }
 }
